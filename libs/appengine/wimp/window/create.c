0b13586e53f2e4853d8c00b35d4c729fcc5dbd83
David Thomas <dave@davespace.co.uk>
2011-12-14 23:24:15 +0000

    Correctly warn of template not found rather than pretending it's an out-of-memory situation.
    git-svn-id: svn://svn.riscos.info/privateeye/trunk@106 befe153f-808b-4961-b90b-b6e528e3d82e

--- libs/appengine/wimp/window/create.c
+++ libs/appengine/wimp/window/create.c
@@ -22,7 +22,7 @@ wimp_w window_create(const char *template_name)
   if (window_find_template_size(template_name,
                                &window_size,
                                &indirected_size) < 0)
-    goto failure;
+    goto notfound;
 
   /* claiming indirected_block then window_block allows more free space to
    * be reclaimed than claiming the other way around */
@@ -31,7 +31,7 @@ wimp_w window_create(const char *template_name)
   {
     indirected_block = malloc(indirected_size);
     if (indirected_block == NULL)
-      goto failure;
+      goto oom;
   }
   else
   {
@@ -40,24 +40,34 @@ wimp_w window_create(const char *template_name)
 
   window_block = malloc(window_size);
   if (window_block == NULL)
-    goto failure;
+    goto oom;
 
   if (window_load_template(template_name,
                            window_block,
                            indirected_block,
                            indirected_size) < 0)
-    goto failure;
+    goto notfound;
 
   handle = window_create_from_memory(window_block);
   if (handle == NULL)
-    goto failure;
+    goto oom;
 
   free(window_block);
 
   return handle; /* no error */
 
 
-failure:
+notfound:
+
+  free(window_block);
+  free(indirected_block);
+
+  error__fatal1("NoAny", template_name); /* fatal error: <name> not found */
+
+  return 0;
+
+
+oom:
 
   free(window_block);
   free(indirected_block);
