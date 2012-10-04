3b60ce892d1217825ed3b5656a436a720e50169c
David Thomas <dave@davespace.co.uk>
2012-10-04 22:08:25 +0000

    Add array_grow() and array_shrinkwrap().
    array_grow is an attempt to factor out all of the doubling strategy growable
    array code I seem to write again and again.
    
    array_shrinkwrap is the counterpart function which resizes a growable array
    to hold only its occupied elements.
    
    
    
    git-svn-id: svn://svn.riscos.info/privateeye/trunk@191 befe153f-808b-4961-b90b-b6e528e3d82e

--- libs/appengine/datastruct/array.h
+++ libs/appengine/datastruct/array.h
@@ -14,6 +14,8 @@
 
 #include <stdlib.h>
 
+/* ----------------------------------------------------------------------- */
+
 /* Delete the specified element. */
 void array_delete_element(void  *array,
                           size_t elemsize,
@@ -27,6 +29,8 @@ void array_delete_elements(void  *array,
                            int    nelems,
                            int    first_doomed,
                            int    last_doomed);
+
+/* ----------------------------------------------------------------------- */
 
 /* Take the contents of an array which used to have elements 'oldwidth' bytes
  * wide and adjust them so they are 'newwidth' bytes wide. Set new bytes to
@@ -58,5 +62,46 @@ void array_squeeze2(unsigned char *base,
 /* Temporary defines until the above functions are renamed. */
 #define array_stretch array_stretch2
 #define array_squeeze array_squeeze2
+
+/* ----------------------------------------------------------------------- */
+
+/**
+ * Grow a dynamically allocated array as required by doubling.
+ *
+ * 'block' can be NULL to perform an initial alloc.
+ * Start with used == allocated == 0.
+ *
+ * \param block     Pointer to pointer to block. Updated on exit.
+ * \param elemsize  Element size.
+ * \param used      Number of currently occupied elements.
+ * \param allocated Pointer to number of allocated elements. Updated on exit.
+ * \param need      Number of unoccupied elements we need.
+ * \param minimum   Minimum number of elements to allocate.
+ *
+ * \return 0 - ok, 1 - out of memory
+ */
+int array_grow(void   **block,
+               size_t   elemsize,
+               int      used,
+               int     *allocated,
+               int      need,
+               int      minimum);
+
+/**
+ * Resize dynamically allocated array to hold exactly the elements it uses.
+ *
+ * \param block     Pointer to pointer to block. Updated on exit.
+ * \param elemsize  Element size.
+ * \param used      Number of currently occupied elements.
+ * \param allocated Pointer to number of allocated elements. Updated on exit.
+ *
+ * \return 0 - ok, 1 - out of memory
+ */
+int array_shrinkwrap(void   **block,
+                     size_t   elemsize,
+                     int      used,
+                     int     *allocated);
+
+/* ----------------------------------------------------------------------- */
 
 #endif /* APPENGINE_ARRAY_H */
