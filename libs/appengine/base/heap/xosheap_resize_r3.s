611dc40358f72d42ea13f9d581cc80b9b2992704
David Thomas <dave@davespace.co.uk>
2017-02-04 20:55:55 +0000

    Replace MOVS pc,r12 instruction, now faulted on Cortex-A53, with MOV pc,r12.
    git-svn-id: svn://svn.riscos.info/privateeye/trunk@210 befe153f-808b-4961-b90b-b6e528e3d82e

--- libs/appengine/base/heap/xosheap_resize_r3.s
+++ libs/appengine/base/heap/xosheap_resize_r3.s
@@ -10,12 +10,12 @@
 
 xosheap_resize_r3
 	MOV	r12, r14
-	MOV	r3, r1
-	MOV	r1, r0
-	MOV	r0, #5
-	SWI	&2001D 		; XOS_Heap
-	STR	r3, [r2] 	; even if it failed
+	MOV	r3, r1		; size increase, or -ve for decrease
+	MOV	r1, r0		; pointer to heap
+	MOV	r0, #5		; reason: increase size of heap
+	SWI	&2001D		; XOS_Heap
+	STR	r3, [r2]	; save actual change in size, even if V set
 	MOVVC	r0, #0
-	MOVS	pc, r12
+	MOV	pc, r12
 
 	END
