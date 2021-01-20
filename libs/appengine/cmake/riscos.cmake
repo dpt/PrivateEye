106142fb972efad7d8f7a71b4c20bb2d0eb3e044
David Thomas <dave@davespace.co.uk>
2021-01-20 23:14:06 +0000

    AppEngine CMake: Sync with fixes from DPTLib

--- libs/appengine/cmake/riscos.cmake
+++ libs/appengine/cmake/riscos.cmake
@@ -9,6 +9,9 @@ set(CMAKE_C_COMPILER ${crossbin}/${prefix}gcc)
 set(CMAKE_ASM_ASASM_FLAGS "" CACHE STRING "" FORCE)
 set(CMAKE_C_FLAGS "-mlibscl -mhard-float -mpoke-function-name" CACHE STRING "" FORCE)
 
+# I'd like to set CMAKE_EXECUTABLE_SUFFIX to ,ff8 here but
+# CMakeGenericSystem.cmake splats it.
+
 set(CMAKE_FIND_ROOT_PATH ${crossbin})
 
 # Search for programs only in the build host directories
