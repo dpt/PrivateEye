85dfafddbaa35976449d214d9858e072623fbeb5
David Thomas <dave@davespace.co.uk>
2021-01-30 23:39:28 +0000

    Remove references to tests for code migrated to DPTLib

--- libs/appengine/test/main/main.c
+++ libs/appengine/test/main/main.c
@@ -8,23 +8,10 @@
 typedef int (testfn)(void);
 
 extern int array_test(void);
-extern int atom_test(void);
-extern int bitarr_test(void);
-extern int bitvec_test(void);
-extern int hash_test(void);
-extern int list_test(void);
-extern int ntree_test(void);
-extern int vector_test(void);
-extern int tagdb_test(void);
-extern int txtfmt_test(void);
 extern int filing_test(void);
 extern int font_test(void);
-extern int lang_test(void);
-extern int packer_test(void);
-extern int layout_test(void);
-extern int stream_test(void);
 extern int pack_test(void);
-extern int pickle_test(void);
+extern int txtfmt_test(void);
 extern int wire_test(void);
 
 typedef struct test
@@ -37,23 +24,10 @@ test;
 static const test tests[] =
 {
   { "array",  array_test  },
-  { "atom",   atom_test   },
-  { "bitarr", bitarr_test },
-  { "bitvec", bitvec_test },
-  { "hash",   hash_test   },
-  { "list",   list_test   },
-  { "ntree",  ntree_test  },
-  { "vector", vector_test },
-  { "tagdb",  tagdb_test  },
-  { "txtfmt", txtfmt_test },
   { "filing", filing_test },
   { "font",   font_test   },
-  { "lang",   lang_test   },
-  { "packer", packer_test },
-  { "layout", layout_test },
-  { "stream", stream_test },
   { "pack",   pack_test   },
-  { "pickle", pickle_test },
+  { "txtfmt", txtfmt_test },
   { "wire",   wire_test   },
 };
 
